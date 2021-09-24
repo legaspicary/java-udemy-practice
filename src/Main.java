1d2c1bff611603de7459374b2740240faa3a6ed8
Cary Legaspi <legaspicary@gmail.com>
2021-09-24 19:48:59 +0800

    Add BarkingDog solution

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		SpeedConverter.printConversion(1.54);
 		MegaBytesConverter.printMegaBytesAndKiloBytes(2500);
+		System.out.println(BarkingDog.shouldWakeUp(true, 8));
 	}
 
 }
