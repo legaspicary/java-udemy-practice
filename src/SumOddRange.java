d32d7a88d9c27f9b66e92c18dd95a993122f8e93
Cary Legaspi <legaspicary@gmail.com>
2021-09-25 19:28:38 +0800

    Add sumOddRange solution

---
+++ src/SumOddRange.java
@@ -0,0 +1,17 @@
+
+public class SumOddRange {
+	public static boolean isOdd(int n) {
+		return n > 0 && n % 2 == 1;
+	}
+
+	public static int sumOdd(int start, int end) {
+		int sum = 0;
+		if (end < start || start <= 0 || end <= 0)
+			return -1;
+		for (int i = start; i <= end; i++) {
+			if (isOdd(i))
+				sum += i;
+		}
+		return sum;
+	}
+}
